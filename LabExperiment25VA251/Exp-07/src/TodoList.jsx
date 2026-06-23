0b80e84da27c7f32e979b1b09c905c92e88578e6
Akash <avivishwakarma7yt@gmail.com>
2026-06-23 09:59:44 +0530

    build todolist

---
+++ LabExperiment25VA251/Exp-07/src/TodoList.jsx
@@ -0,0 +1,20 @@
+import TodoItem from "./TodoItem";
+
+function TodoList({ todos, onToggle, onDelete }) {
+  if (todos.length === 0) return null;
+
+  return (
+    <ul className="todo-list">
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onToggle={onToggle}
+          onDelete={onDelete}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
